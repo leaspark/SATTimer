b2c05dd44c5d04ecac3d0e0abf2f4fc317fac965
leaspark <16lsparkman@castilleja.org>
2015-04-30 11:08:06 -0700

    This is the actual SAT timer that works.

--- SATTimer/FirstViewController.swift
+++ SATTimer/FirstViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var beginExam: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
